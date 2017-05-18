ca5242ebcec0f3b1ebe413678e0e2c09ed55ce1d
Max Zakharzhevskiy <max.za@digitalpersona.com>
2017-05-17 20:07:57 -0700

    Fixed webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 var path = require('path');
 
 module.exports = {
-    entry: "src/index.ts",
+    entry: "./src/index.ts",
     output: {
-        filename: 'bundle.js',
+        filename: 'tops-bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
